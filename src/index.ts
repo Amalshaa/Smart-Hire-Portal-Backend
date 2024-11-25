448ed59fd09f3b95a8d872a74da44ebfa3f511cf
Amalsha <amalsha2001@gmail.com>
2024-11-25 15:45:25 +0530

    Initial backend commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
+import dotenv from "dotenv"
 import express from "express";
+import jobsRouter from "./api/jobs";
+import { connectDB } from "./infrastructure/db";
+import jobApplicationRouter from "./api/jobApplication";
+import cors from "cors";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
-app.listen(8000, () => console.log("Server is listening on port 8000."));
+connectDB();
+
+app.use("/jobs", jobsRouter);
+
+app.use("/jobApplications",jobApplicationRouter)
+
+const PORT = 8000
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
